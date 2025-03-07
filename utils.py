25ffaff2c4b35be1644a87ca854b9c1f789b853d
gangzhi <gangzhi2000@gmail.com>
2025-03-07 02:48:40 -0500

    sp500 totality

--- utils.py
+++ utils.py
@@ -8,37 +8,47 @@ import bs4 as bs
 import pandas as pd
 import matplotlib.pyplot as plt
 
+from constants import ALL_MEASURES
+
+
 conn = sqlite3.connect("stock_screener.db")
 cur = conn.cursor()
-cur.execute("CREATE TABLE IF NOT EXISTS fundamental(ticker, info, lastmod)")
+cur.execute("CREATE TABLE IF NOT EXISTS equity_info(ticker, info, lastmod)")
 conn.commit()
 
-def update_spy_components_info():
+def get_ticker_info(ticker):
+    ticker = ticker.upper()
+    res = cur.execute('SELECT * FROM equity_info WHERE ticker =?', (ticker,))
+    cached = res.fetchone()
+    if cached and cached[2] == date.today().isoformat():
+        return json.loads(cached[1])
+    stock = yf.Ticker(ticker)
+    if not stock or not hasattr(stock, 'info'):
+        return None
+    info = stock.info
+    cur.execute('INSERT OR REPLACE INTO equity_info VALUES(?, ?, ?)',
+        (ticker, json.dumps(info), date.today().isoformat()))
+    conn.commit()
+    return info
+    
+def spy_components():
+    result = []
     resp = requests.get('https://stockanalysis.com/list/sp-500-stocks/')
     soup = bs.BeautifulSoup(resp.text, 'html')
     table = soup.find('table', {'id': 'main-table'})
-    tickers = []
-
     for row in table.find_all('tr')[1:]:
         fields = row.find_all('td')
         if not fields:
             continue
         ticker = row.find_all('td')[1].text
-        res = cur.execute('SELECT * FROM fundamental WHERE ticker =?', (ticker,))
-        cached = res.fetchone()
-        if cached is None or cached[2] != date.today().isoformat():
-            info = yf.Ticker(ticker).info
-            cur.execute('INSERT OR REPLACE INTO fundamental VALUES(?, ?, ?)',
-                        (ticker, json.dumps(info), date.today().isoformat()))
-            conn.commit()
-        tickers.append(ticker)    
+        result.append(ticker)
+    return result
 
-    return tickers
-
-
-def init_db():
-
-    return cur
-
-
-
+def toDF(stocks):
+    infos = [get_ticker_info(stock) for stock in stocks]
+    infos = [x for x in infos if x is not None]
+    row_keys = ['symbol', 'industryKey','sectorKey']
+    df = pd.DataFrame(infos, columns=row_keys + ALL_MEASURES)
+    df.set_index('symbol', inplace=True)
+    return df
+     
